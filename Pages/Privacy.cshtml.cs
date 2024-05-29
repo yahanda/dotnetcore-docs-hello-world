1c09e8f827358289804d6f4387f64d45079bf494
Yasuhiro Handa <58975407+yahanda@users.noreply.github.com>
2024-05-29 22:33:20 +0900

    Add 5min sleep

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -7,6 +7,9 @@ public class PrivacyModel : PageModel
 {
     private readonly ILogger<PrivacyModel> _logger;
 
+    // Add 5min sleep to simulate long running application
+    System.Threading.Thread.Sleep(300000);
+
     public PrivacyModel(ILogger<PrivacyModel> logger)
     {
         _logger = logger;
